f2d7aad5dea8709d1890c60c95b85423a5f9e8dd
Dylan648 <dbolter03@gmail.com>
2021-09-07 13:25:13 -0500

    started UI and created popup

---
+++ task_manager/EditTodayGoal.swift
@@ -0,0 +1,20 @@
+//
+//  EditTodayGoal.swift
+//  task_manager
+//
+//  Created by Dylan Bolter on 9/7/21.
+//
+
+import SwiftUI
+
+struct EditTodayGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditTodayGoal_Previews: PreviewProvider {
+    static var previews: some View {
+        EditTodayGoal()
+    }
+}
